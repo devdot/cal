1f9a65f964f4149578bd4484c41a91b5953cf90e
Thomas Schaller <git@thomasschaller.eu>
2016-08-20 23:27:37 +0200

    Added frame for the default view (cal), css file for the component backend

--- admin/views/cal/tmpl/default.php
+++ admin/views/cal/tmpl/default.php
@@ -9,5 +9,20 @@
  
 // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
+
 ?>
-<h1>Cal</h1>
+<form id="adminForm" action="?option=com_cal&view=event" method="post" name="adminForm">
+    <div id="j-sidebar-container" class="span2">
+        <?php echo $this->sidebar; ?>
+    </div>
+    <div id="j-main-container" class="span10 j-toggle-main">
+        <div class="span6">
+            <h2>Upcoming Events</h2>
+
+        </div>
+        <div class="span6">
+            <h2>New Events</h2>
+        </div>
+    </div>
+    <input name="task" value="" type="hidden">
+</form>
--- admin/views/cal/view.html.php
+++ admin/views/cal/view.html.php
@@ -15,8 +15,10 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class calViewCal extends JViewLegacy
-{
+class calViewCal extends JViewLegacy {
+    
+    protected $sidebar;
+    
 	/**
 	 * Display the Hello World view
 	 *
@@ -29,7 +31,21 @@ class calViewCal extends JViewLegacy
 		// Get data from the model
 		$this->items		= $this->get('Items');
 		$this->pagination	= $this->get('Pagination');
- 
+        
+        JHtml::stylesheet("/administrator/components/com_cal/css/cal.css");
+        
+        JToolbarHelper::title('Calendar', 'calendar');
+        //JToolbarHelper::preferences('com_cal');
+        JToolbarHelper::addNew();
+        
+        JHtmlSidebar::addEntry("Overview", "?option=com_cal", true);
+        JHtmlSidebar::addEntry("Event List", "?option=com_cal&view=eventlist");
+        JHtmlSidebar::addEntry("Locations", "?option=com_cal&view=locations");
+        JHtmlSidebar::addEntry("Categories", "?option=com_cal&view=categories");
+        JHtmlSidebar::addEntry("Archive", "?option=com_cal&view=archive");
+        JHtmlSidebar::addEntry("Options", "?option=com_cal&view=options");
+        
+        
 		// Check for errors.
 		if (count($errors = $this->get('Errors')))
 		{
@@ -38,6 +54,8 @@ class calViewCal extends JViewLegacy
 			return false;
 		}
  
+        
+        $this->sidebar = JHtmlSidebar::render();
 		// Display the template
 		parent::display($tpl);
 	}
