ef1b60dbafd2faded4e27e93ddde4dccb3c34981
Thomas Schaller <git@thomasschaller.eu>
2016-08-20 14:11:19 +0200

    Added database work

--- admin/models/helloworld.php
+++ admin/models/helloworld.php
@@ -6,31 +6,68 @@
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+ 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
  
 /**
- * HelloWorldList Model
+ * HelloWorld Model
  *
  * @since  0.0.1
  */
-class HelloWorldModelHelloWorlds extends JModelList
+class HelloWorldModelHelloWorld extends JModelItem
 {
 	/**
-	 * Method to build an SQL query to load the list data.
-	 *
-	 * @return      string  An SQL query
+	 * @var array messages
 	 */
-	protected function getListQuery()
+	protected $messages;
+ 
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $type    The table name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  JTable  A JTable object
+	 *
+	 * @since   1.6
+	 */
+	public function getTable($type = 'HelloWorld', $prefix = 'HelloWorldTable', $config = array())
 	{
-		// Initialize variables.
-		$db    = JFactory::getDbo();
-		$query = $db->getQuery(true);
+		return JTable::getInstance($type, $prefix, $config);
+	}
  
-		// Create the base select statement.
-		$query->select('*')
-                ->from($db->quoteName('#__helloworld'));
+	/**
+	 * Get the message
+	 *
+	 * @param   integer  $id  Greeting Id
+	 *
+	 * @return  string        Fetched String from Table for relevant Id
+	 */
+	public function getMsg($id = 1)
+	{
+		if (!is_array($this->messages))
+		{
+			$this->messages = array();
+		}
  
-		return $query;
+		if (!isset($this->messages[$id]))
+		{
+			// Request the selected id
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+ 
+			// Get a TableHelloWorld instance
+			$table = $this->getTable();
+ 
+			// Load the message
+			$table->load($id);
+ 
+			// Assign the message
+			$this->messages[$id] = $table->greeting;
+		}
+ 
+		return $this->messages[$id];
 	}
 }
