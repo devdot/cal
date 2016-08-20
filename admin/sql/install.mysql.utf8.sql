2d7fedbc1f168dd0819f5e2fd4b7e0b1bb821d50
Thomas Schaller <git@thomasschaller.eu>
2016-08-20 23:52:49 +0200

    Added locations table SQL

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,2 +1,4 @@
 CREATE TABLE IF NOT EXISTS `#__cal_settings` ( `ID` INT NOT NULL AUTO_INCREMENT , `data` TEXT NOT NULL , PRIMARY KEY (`ID`) ) ENGINE = InnoDB;
 INSERT INTO `#__cal_settings` (`ID`, `data`) VALUES (NULL, '{}');
+
+CREATE TABLE `#__cal_locations` ( `ID` INT NOT NULL AUTO_INCREMENT, `Name` VARCHAR(64) NOT NULL , `AddrStreet` VARCHAR(64) NULL , `AddrZip` MEDIUMINT NULL , `AddrCity` VARCHAR(64) NOT NULL , `AddrCountry` VARCHAR(64) NOT NULL , `GeoLoc` POINT NULL , `Link` VARCHAR(256) NOT NULL , `Desc` INT NOT NULL , PRIMARY KEY (`ID`) ) ENGINE = InnoDB;
