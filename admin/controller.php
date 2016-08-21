ee6d0de73316473045dd6cb656ebeb73dd93ae14
Thomas Schaller <git@thomasschaller.eu>
2016-08-21 17:23:40 +0200

    Added display function body to CalController as overwrite. No new functionallity yet.

--- admin/controller.php
+++ admin/controller.php
@@ -25,5 +25,27 @@ class CalController extends JControllerLegacy
 	 * @var string
 	 * @since 12.2
 	 */
-	protected $default_view = 'Cal';
+	protected $default_view = 'cal';
+    
+    public function display($cachable = false, $urlparams = array()) {
+        
+        //JLoader::register('ContactHelper', JPATH_ADMINISTRATOR . '/components/com_contact/helpers/contact.php');
+
+		$view   = $this->input->get('view', 'cal');
+		$layout = $this->input->get('layout', 'default');
+		$id     = $this->input->getInt('id');
+
+		// Check for edit form.
+		//if ($view == 'contact' && $layout == 'edit' && !$this->checkEditId('com_contact.edit.contact', $id)) {
+			// Somehow the person just went to the form - we don't allow that.
+		//	$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
+		//	$this->setMessage($this->getError(), 'error');
+		//	$this->setRedirect(JRoute::_('index.php?option=com_contact&view=contacts', false));
+
+		//	return false;
+		//}
+
+		return parent::display();
+        
+    }
 }
