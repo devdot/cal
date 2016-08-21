3cd82ceaa5422d049ed5e6e471b29c1222673c4d
Thomas Schaller <git@thomasschaller.eu>
2016-08-21 23:01:29 +0200

    Added basic location controller

---
+++ admin/controllers/location.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_contact
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
+
+/**
+ * Controller for a single contact
+ *
+ * @since  1.6
+ */
+class CalControllerLocation extends JControllerForm {
+	
+}
