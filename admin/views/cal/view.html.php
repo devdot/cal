f970d51ea97a669d10944580a9bcd93fab67a64a
Thomas Schaller <git@thomasschaller.eu>
2016-08-22 17:55:55 +0200

    Revert "Changed calViewCal to CalViewCal for uniformity"
    This reverts commit 89b645bf031b562a17b75cbbd1d7f29f1a940ecd.

--- admin/views/cal/view.html.php
+++ admin/views/cal/view.html.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class CalViewCal extends JViewLegacy {
+class calViewCal extends JViewLegacy {
     
     protected $sidebar;
     
