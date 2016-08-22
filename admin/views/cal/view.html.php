26d85a2efb19c38c38176d1ab0d5dcd7c7b2705b
Thomas Schaller <git@thomasschaller.eu>
2016-08-22 18:21:32 +0200

    Renamed to CalViewCal for uniform class names.

--- admin/views/cal/view.html.php
+++ admin/views/cal/view.html.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class calViewCal extends JViewLegacy {
+class CalViewCal extends JViewLegacy {
     
     protected $sidebar;
     
