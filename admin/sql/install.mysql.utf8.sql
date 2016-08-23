fbc0ebfe6385e3db6e6e5380a95571a50927e372
Thomas Schaller <git@thomasschaller.eu>
2016-08-23 15:14:45 +0200

    Implemented events SQL query (Hope I won't have to change this a thousand times)

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -17,3 +17,28 @@ CREATE TABLE IF NOT EXISTS `#__cal_locations` (
 
 CREATE TABLE IF NOT EXISTS `#__cal_resources` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(64) NOT NULL , `catid` INT NOT NULL DEFAULT '0' , `type` TINYINT(4) NOT NULL DEFAULT '0' , `type_id` INT DEFAULT NULL , `description` TEXT NOT NULL , PRIMARY KEY (`id`) ) ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `#__cal_events` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `start` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `end` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `introtext` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fulltext` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `state` tinyint(3) NOT NULL DEFAULT '0',
+  `catid` int(10) unsigned NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(10) unsigned NOT NULL DEFAULT '0',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(10) unsigned NOT NULL DEFAULT '0',
+  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `metakey` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `metadesc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access` int(10) unsigned NOT NULL DEFAULT '0',
+  `location_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `recurring_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `recurring_schedule` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
