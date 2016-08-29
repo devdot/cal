f0436019c421c92e9aded8a301d94fb1954c7ac7
Thomas Schaller <git@thomasschaller.eu>
2016-08-29 18:15:24 +0200

    Remade the whole install SQL

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,24 +1,9 @@
--- CREATE TABLE IF NOT EXISTS `#__cal_settings` ( `ID` INT NOT NULL AUTO_INCREMENT , `data` TEXT NOT NULL , PRIMARY KEY (`ID`) ) ENGINE = InnoDB;
-INSERT INTO `#__cal_settings` (`ID`, `data`) VALUES (NULL, '{}');
-
-CREATE TABLE IF NOT EXISTS `#__cal_locations` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  `addrStreet` varchar(64) DEFAULT NULL,
-  `addrZip` varchar(5) DEFAULT NULL,
-  `addrCity` varchar(64) NOT NULL,
-  `addrCountry` varchar(64) NOT NULL,
-  `geoLoc` point DEFAULT NULL,
-  `link` varchar(256) NOT NULL,
-  `desc` text NOT NULL,
-  `published` bit(1) NOT NULL DEFAULT b'1',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `#__cal_resources` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(64) NOT NULL , `catid` INT NOT NULL DEFAULT '0' , `type` TINYINT(4) NOT NULL DEFAULT '0' , `type_id` INT DEFAULT NULL , `description` TEXT NOT NULL , PRIMARY KEY (`id`) ) ENGINE = InnoDB;
+--
+-- Table structure for table `#__cal_events`
+--
 
 CREATE TABLE IF NOT EXISTS `#__cal_events` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `start` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `end` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -39,6 +24,69 @@ CREATE TABLE IF NOT EXISTS `#__cal_events` (
   `location_id` int(10) unsigned NOT NULL DEFAULT '0',
   `recurring_id` int(10) unsigned NOT NULL DEFAULT '0',
   `recurring_schedule` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `#__cal_events`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `#__cal_events`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+  
+--
+-- Table structure for table `#__cal_events_resources`
+--
+
+CREATE TABLE IF NOT EXISTS `#__cal_events_resources` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `#__cal_events_resources`
+  ADD PRIMARY KEY (`id`), ADD KEY `event_id` (`event_id`), ADD KEY `resource_id` (`resource_id`);
+  
+  ALTER TABLE `#__cal_events_resources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+  
+--
+-- Table structure for table `#__cal_locations`
+--
+
+CREATE TABLE IF NOT EXISTS `#__cal_locations` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `addrStreet` varchar(64) DEFAULT NULL,
+  `addrZip` varchar(5) DEFAULT NULL,
+  `addrCity` varchar(64) NOT NULL,
+  `addrCountry` varchar(64) NOT NULL,
+  `geoLoc` point DEFAULT NULL,
+  `link` varchar(256) NOT NULL,
+  `desc` text NOT NULL,
+  `published` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `#__cal_locations`
+  ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `#__cal_locations`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+  
+--
+-- Table structure for table `#__cal_resources`
+--
+
+CREATE TABLE IF NOT EXISTS `#__cal_resources` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `catid` int(11) NOT NULL DEFAULT '0',
+  `type` tinyint(4) NOT NULL DEFAULT '0',
+  `type_id` int(11) DEFAULT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `#__cal_resources`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `#__cal_resources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
