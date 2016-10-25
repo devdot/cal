b4d24001a9e63e3d803cc88a5ab789be7b019e1d
Thomas Schaller <git@thomasschaller.eu>
2016-10-25 17:19:44 +0200

    Added CalHelper for site (copy pasta from admin)

---
+++ site/helpers/cal.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cal
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * Cal component helper.
+ *
+ * @param   string  $submenu  The name of the active view.
+ *
+ * @return  void
+ *
+ * @since   1.6
+ */
+abstract class CalHelper {
+	/**
+	 * Configure the Linkbar.
+	 *
+	 * @return Bool
+	 */
+
+	
+	 public static function getTimeZone() {
+		 //first the user timezone
+		 //if there is no user-set timezone, use the server timezone
+        $userTz = JFactory::getUser()->getParam('timezone');
+        $timeZone = JFactory::getConfig()->get('offset');
+        if($userTz) {
+            $timeZone = $userTz;
+        }
+        return new DateTimeZone($timeZone);
+    }
+}
