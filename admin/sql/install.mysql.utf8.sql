79181d1322c05f4e00fdda3e5a784852752950e2
Thomas Schaller <git@thomasschaller.eu>
2016-11-24 20:32:48 +0100

    Added cal_archive table creation SQL

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -32,6 +32,42 @@ ALTER TABLE `#__cal_events`
 
 ALTER TABLE `#__cal_events`
   MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+
+--
+-- Table structure for table `#__cal_archive`
+-- should be the same structure as cal_events 
+-- no autoincrement though (because id's will get copied from cal_events)
+
+CREATE TABLE IF NOT EXISTS `#__cal_archive` (
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `start` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `end` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `introtext` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fulltext` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `state` tinyint(3) NOT NULL DEFAULT '0',
+  `catid` int(10) unsigned NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(10) unsigned NOT NULL DEFAULT '0',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(10) unsigned NOT NULL DEFAULT '0',
+  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `metakey` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `metadesc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access` int(10) unsigned NOT NULL DEFAULT '0',
+  `location_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `recurring_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `recurring_schedule` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `#__cal_archive`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `#__cal_archive`
+  MODIFY `id` int(10) unsigned NOT NULL;
   
 --
 -- Table structure for table `#__cal_events_resources`
