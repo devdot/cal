52dc83cda0a9cde99bad87d82c2af3d61cfcb182
Thomas Schaller <git@thomasschaller.eu>
2017-03-08 09:42:08 +0100

    Moved JS into the modules JS file

---
+++ mod_cal_small/js/mod_cal_small.js
@@ -0,0 +1,52 @@
+mod_cal_small_next_active = true;
+mod_cal_small_prev_active = true;
+	
+function mod_cal_small() {
+	jQuery('.mod-cal-small-inner').scrollTop(0);
+	cscroll = jQuery('.mod-cal-small-active').offset().top - jQuery('.mod-cal-small-inner').offset().top;
+	jQuery('.mod-cal-small-inner').scrollTop(cscroll);
+}
+function mod_cal_small_next() {
+	if(!mod_cal_small_next_active)
+		return;
+
+	h = jQuery('.mod-cal-small-active').height();
+	jQuery('.mod-cal-small-inner').scrollTop(jQuery('.mod-cal-small-inner').scrollTop() + h);
+
+	old = jQuery('.mod-cal-small-active');
+	old.next().addClass('mod-cal-small-active');
+	old.removeClass('mod-cal-small-active');
+
+	mod_cal_small_update();
+
+}
+function mod_cal_small_prev() {
+	if(!mod_cal_small_prev_active)
+		return;
+
+	h = jQuery('.mod-cal-small-active').height();
+	jQuery('.mod-cal-small-inner').scrollTop(jQuery('.mod-cal-small-inner').scrollTop() - h);
+
+	old = jQuery('.mod-cal-small-active');
+	old.prev().addClass('mod-cal-small-active');
+	old.removeClass('mod-cal-small-active');
+
+	mod_cal_small_update();
+}
+
+function mod_cal_small_update() {
+	mod_cal_small_next_active = jQuery('.mod-cal-small-active').next().parent().hasClass('mod-cal-small-inner');
+	mod_cal_small_prev_active = jQuery('.mod-cal-small-active').prev().parent().hasClass('mod-cal-small-inner');
+
+	if(mod_cal_small_next_active)
+		jQuery('.mod-cal-small-next').removeClass('inactive');
+	else
+		jQuery('.mod-cal-small-next').addClass('inactive');
+
+	if(mod_cal_small_prev_active)
+		jQuery('.mod-cal-small-prev').removeClass('inactive');
+	else
+		jQuery('.mod-cal-small-prev').addClass('inactive');
+}
+	
+	
--- mod_cal_small/tmpl/default.php
+++ mod_cal_small/tmpl/default.php
@@ -1,7 +1,10 @@
 <?php 
 // No direct access
 
-defined('_JEXEC') or die;?>
+defined('_JEXEC') or die;
+
+JFactory::getDocument()->addScript('modules/mod_cal_small/js/mod_cal_small.js');
+?>
 <div class="mod-cal-small">
 	<h2 class="mod-cal-small-headline">Nächste Veranstaltungen</h2>
 	<div class="mod-cal-small-outer">
@@ -109,55 +112,6 @@ defined('_JEXEC') or die;?>
 	</div>
 </div>
 <script>
-	jQuery('.mod-cal-small-inner').scrollTop(0);
-	cscroll = jQuery('.mod-cal-small-active').offset().top - jQuery('.mod-cal-small-inner').offset().top;
-	jQuery('.mod-cal-small-inner').scrollTop(cscroll);
-	
-	mod_cal_small_next_active = true;
-	mod_cal_small_prev_active = true;
-	
-	function mod_cal_small_next() {
-		if(!mod_cal_small_next_active)
-			return;
-		
-		h = jQuery('.mod-cal-small-active').height();
-		jQuery('.mod-cal-small-inner').scrollTop(jQuery('.mod-cal-small-inner').scrollTop() + h);
-		
-		old = jQuery('.mod-cal-small-active');
-		old.next().addClass('mod-cal-small-active');
-		old.removeClass('mod-cal-small-active');
-		
-		mod_cal_small_update();
-		
-	}
-	function mod_cal_small_prev() {
-		if(!mod_cal_small_prev_active)
-			return;
-		
-		h = jQuery('.mod-cal-small-active').height();
-		jQuery('.mod-cal-small-inner').scrollTop(jQuery('.mod-cal-small-inner').scrollTop() - h);
-		
-		old = jQuery('.mod-cal-small-active');
-		old.prev().addClass('mod-cal-small-active');
-		old.removeClass('mod-cal-small-active');
-		
-		mod_cal_small_update();
-	}
-	
-	function mod_cal_small_update() {
-		mod_cal_small_next_active = jQuery('.mod-cal-small-active').next().parent().hasClass('mod-cal-small-inner');
-		mod_cal_small_prev_active = jQuery('.mod-cal-small-active').prev().parent().hasClass('mod-cal-small-inner');
-		
-		if(mod_cal_small_next_active)
-			jQuery('.mod-cal-small-next').removeClass('inactive');
-		else
-			jQuery('.mod-cal-small-next').addClass('inactive');
-		
-		if(mod_cal_small_prev_active)
-			jQuery('.mod-cal-small-prev').removeClass('inactive');
-		else
-			jQuery('.mod-cal-small-prev').addClass('inactive');
-	}
-	
+	mod_cal_small();
 	mod_cal_small_update();
 </script>
