5ae79db8165505e10f9b529cfe46e1e15b329894
Thomas Schaller <git@thomasschaller.eu>
2017-05-01 13:27:08 +0200

    Basic CT admin view

---
+++ admin/views/ct/tmpl/default.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cal
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+JFactory::getDocument()->addStyleSheet(JURI::base().'components/com_cal/css/cal.css');
+
+?>
+<form id="adminForm" action="?option=com_cal&view=ct" method="post" name="adminForm">
+    <div id="j-sidebar-container" class="span2">
+        <?php echo $this->sidebar; ?>
+    </div>
+    <div id="j-main-container" class="span10 j-toggle-main">
+        <div class="span6">
+            <h2><?php echo JText::_('COM_CAL_QUICKACCESS'); ?></h2>
+			<div class="cal-cpanel">
+				<a class="btn" onclick="Joomla.submitbutton('ct.import')">
+					<span class="cal-cpanel-icon"><span class="icon icon-upload"></span></span>
+					<span class="cal-cpanel-title"><?php echo JText::_("COM_CAL_CPANEL_CT_IMPORT"); ?></span>
+				</a>
+				<a class="btn" onclick="Joomla.submitbutton('ct.tokenGenerator')">
+					<span class="cal-cpanel-icon"><span class="icon icon-wand"></span></span>
+					<span class="cal-cpanel-title"><?php echo JText::_("COM_CAL_CPANEL_CT_TOKEN_GENERATOR"); ?></span>
+				</a>
+			</div>
+        </div>
+        <div class="span6">
+            
+        </div>
+    </div>
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="boxchecked" value="0" />
+    <?php echo JHtml::_('form.token'); ?>
+</form>
---
+++ admin/views/ct/view.html.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cal
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+JLoader::register('CalHelperCT', JPATH_COMPONENT . '/helpers/ct.php');
+
+/**
+ * Cal ChurchTools View
+ *
+ * @since  0.0.1
+ */
+class CalViewCT extends JViewLegacy {
+    
+    protected $sidebar;
+    public $state;
+    public $items;
+	
+	public $ct;
+    
+	/**
+	 * Display the ChurchTools View
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null) {
+		// Get data from the model
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+        $this->state        = $this->get('State');
+		
+        JToolbarHelper::title('Calendar / ChurchTools', 'calendar');
+        JToolbarHelper::addNew('event.add');
+        
+        CalHelper::addSubmenu('ct');
+        
+		$this->ct = CalHelperCT::getInstance();
+        
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+ 
+			return false;
+		}
+ 
+        
+        $this->sidebar = JHtmlSidebar::render();
+		// Display the template
+		parent::display($tpl);
+	}
+}
