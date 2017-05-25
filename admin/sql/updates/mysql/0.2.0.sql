1717c1f0f751857c2ce57894e8da175aabc94267
Thomas Schaller <git@thomasschaller.eu>
2017-05-25 14:01:15 +0200

    SQL can be hard.

--- admin/sql/updates/mysql/0.2.0.sql
+++ admin/sql/updates/mysql/0.2.0.sql
@@ -1,4 +1,4 @@
 -- remove resource tables
-DROP TABLE `#__cal_events_resources`
-DROP TABLE `#__cal_resources`
-DROP TABLE `#__cal_archive_resources`
+DROP TABLE `#__cal_events_resources`;
+DROP TABLE `#__cal_resources`;
+DROP TABLE `#__cal_archive_resources`;
