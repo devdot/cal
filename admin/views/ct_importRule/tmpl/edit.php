b6a3a7e03518578e48cda7393338ad96193b0e45
Thomas Schaller <git@thomasschaller.eu>
2017-05-25 12:21:52 +0200

    Implemented view for CT Import Rule. Ain't pretty but working.

---
+++ admin/views/ct_importRule/tmpl/edit.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cal
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.formvalidator');
+JHtml::_('behavior.keepalive');
+JHtml::_('formbehavior.chosen', 'select');
+
+JFactory::getDocument()->addScriptDeclaration('
+	Joomla.submitbutton = function(task) {
+		if (task == "ct_importRule.cancel" || document.formvalidator.isValid(document.getElementById("cal-ct-importRule-form"))) {
+			Joomla.submitform(task, document.getElementById("cal-ct-importRule-form"));
+
+			if (task !== "ct_importRule.apply") {
+				window.parent.jQuery("#cal-ct-importRule-form' . $this->item->id . 'Modal").modal("hide");
+			}
+		}
+	};
+');
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_cal&view=ct_importRule&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" class="form-validate" id="cal-ct-importRule-form">
+
+	<?php echo JLayoutHelper::render('joomla.edit.title_alias', $this); ?>
+
+	<div class="form-horizontal">
+		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
+
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', JText::_('COM_CAL_EDIT_LOCATION')); ?>
+		<div class="row-fluid">
+			<div class="span9">
+				<?php echo $this->form->renderField('rules'); ?>	
+			</div>
+			<div class="span3">
+				<?php echo $this->form->renderField('state'); ?>
+			</div>
+		</div>
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
+
+		<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+	</div>
+	<input type="hidden" name="task" value="" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
---
+++ admin/views/ct_importRule/view.html.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cal
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * View for editing a single CT import rule
+ *
+ * @since  0.0.1
+ */
+class CalViewCT_ImportRule extends JViewLegacy {
+    
+    protected $form;
+	protected $item;
+	protected $state;
+    
+	/**
+	 * 
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		
+		$this->item  = $this->get('Item');
+		$this->form  = $this->get('Form');
+		$this->state = $this->get('State');
+        
+        
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+ 
+			return false;
+		}
+		
+		JFactory::getApplication()->input->set("hidemainmenu", true);
+		
+		JToolbarHelper::title('Calendar / ChurchTools Import Rule', 'calendar');
+		
+		//toolbar elements
+		JToolbarHelper::apply('ct_importRule.apply');
+		JToolbarHelper::save('ct_importRule.save');
+		JToolbarHelper::save2new('ct_importRule.save2new');
+		
+		//this isn't a new item, otherwise the id wouldn't be 0
+		if($this->item->id != 0) {
+			JToolbarHelper::save2copy('ct_importRule.save2copy');
+		}
+		JToolbarHelper::cancel('ct_importRule.cancel');
+		
+		// Display the template
+		parent::display($tpl);
+	}
+}
