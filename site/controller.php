d52e4a8c245fd7313535a6833afd020d01c73662
Thomas Schaller <mail@thomasschaller.eu>
2018-03-06 19:14:52 +0100

    Controller now checks, whether the current event has a wrong link and redirects to the correct link (as generated in the router, there are still dublicates)

--- site/controller.php
+++ site/controller.php
@@ -15,6 +15,28 @@ defined('_JEXEC') or die('Restricted access');
  */
 class CalController extends JControllerLegacy {
 	function display($cachable = false, $urlparams = array()) {
+		// get the view
+		$view = JFactory::getApplication()->input->getCmd('view', 'events');
+		
+		// check for event view
+		if($view == 'event') {
+			$this->checkAndHandleEventAlias();
+		}
+		
 		parent::display(true); //true asks for caching.
+	}
+	
+	private function checkAndHandleEventAlias() {
+		// first get all the data we need
+		$app = JFactory::getApplication();
+		$id = $app->input->getCmd('id');
+		$format = $app->input->getCmd('format', 'html');
+		
+		// now check against router (might be slow)
+		$route = JRoute::_('index.php?option=com_cal&view=event&id='.$id.'&format='.$format);
+		if($route != JFactory::getURI()->getPath()) {
+			// let's redirect to the correct page
+			$this->setRedirect($route);
+		}
 	}
 }
